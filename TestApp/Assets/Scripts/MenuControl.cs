cf540d863c315f72809b163b92ed0233cb21c621
Devilisch <kirowatanabe@gmail.com>
2019-12-01 04:16:15 +0300

    ver1.0.4 - rework 1-3 levels, add main menu scene, project refactoring

---
+++ TestApp/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+    public GameObject StartMenu;
+    public GameObject InfoMenu;
+    public GameObject ExitMenu;
+
+    public void ShowInfoMenu() {
+        StartMenu.SetActive(false);
+        InfoMenu.SetActive(true);
+    }
+
+    public void ShowExitMenu() {
+        StartMenu.SetActive(false);
+        ExitMenu.SetActive(true);
+    }
+
+    public void ShowStartMenu() {
+        InfoMenu.SetActive(false);
+        ExitMenu.SetActive(false);
+        StartMenu.SetActive(true);
+    }
+
+    public void ExitFromTheGame() {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
